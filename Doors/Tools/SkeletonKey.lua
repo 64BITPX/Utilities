877bb7ba57283872f3cf97cf58d45a109bff41ab
64BITPX <127034870+64BITPX@users.noreply.github.com>
2023-03-05 22:45:55 +0700

    Update SkeletonKey.lua

--- Doors/Tools/SkeletonKey.lua
+++ Doors/Tools/SkeletonKey.lua
@@ -9,7 +9,7 @@ local exampleTool = loadstring(game:HttpGet("https://raw.githubusercontent.com/V
 CustomShop.CreateItem(exampleTool, {
     Title = "Skeleton Key",
     Desc = "Five uses,Holds secrets",
-    Image = "https://raw.githubusercontent.com/VeaselGod/Utilities1/main/SkeletonKey.lua",
-    Price = 200,
+    Image = "https://static.wikia.nocookie.net/doors-game/images/e/eb/Icon_skeletonkey2.png/revision/latest?cb=20220728033038",
+    Price = 100,
     Stack = 1,
 })
