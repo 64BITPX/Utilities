9213b62ce4c846860cd4b0296d1e1166714e0790
64BITPX <127034870+64BITPX@users.noreply.github.com>
2023-03-06 11:12:56 +0700

    Update SkeletonKey.lua

--- Doors/Tools/SkeletonKey.lua
+++ Doors/Tools/SkeletonKey.lua
@@ -1,9 +1,16 @@
+--[[
+
+    NOTE: Custom shop items will NOT cost any real knobs
+
+]]--
+
 local Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
 local CustomShop = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Shop%20Items/Source.lua"))()
 
 
 -- Create your tool here
 local exampleTool = loadstring(game:HttpGet("https://raw.githubusercontent.com/VeaselGod/Utilities1/main/SkeletonKey.lua", true))()
+
 
 -- Create custom shop item
 CustomShop.CreateItem(exampleTool, {
